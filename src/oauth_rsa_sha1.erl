aa15d4ffd312b64c0d4a096d0dc5d8d5132d2906
Fernando Benavides <fernando.benavides@inakanetworks.com>
2010-09-30 17:25:18 -0300

    ready for R14B

--- src/oauth_rsa_sha1.erl
+++ src/oauth_rsa_sha1.erl
@@ -4,12 +4,14 @@
 
 -include_lib("public_key/include/public_key.hrl").
 
-
+-spec signature(string(), string()) -> string().
 signature(BaseString, PrivateKeyPath) ->
-  {ok, [Info]} = public_key:pem_to_der(PrivateKeyPath),
-  {ok, PrivateKey} = public_key:decode_private_key(Info),
+	{ok, Contents} = file:read_file(PrivateKeyPath),
+  [Info] = public_key:pem_decode(Contents),
+  PrivateKey = public_key:pem_entry_decode(Info),
   base64:encode_to_string(public_key:sign(list_to_binary(BaseString), PrivateKey)).
 
+-spec verify(string(), string(), term()) -> boolean().
 verify(Signature, BaseString, PublicKey) ->
   public_key:verify_signature(to_binary(BaseString), sha, base64:decode(Signature), public_key(PublicKey)).
 
@@ -19,7 +21,8 @@ to_binary(Term) when is_binary(Term) ->
   Term.
 
 public_key(Path) when is_list(Path) ->
-  {ok, [{cert, DerCert, not_encrypted}]} = public_key:pem_to_der(Path),
+	{ok, Contents} = file:read_file(Path),
+  [{cert, DerCert, not_encrypted}] = public_key:pem_decode(Contents),
   {ok, Cert} = public_key:pkix_decode_cert(DerCert, otp),
   public_key(Cert);
 public_key(#'OTPCertificate'{tbsCertificate=Cert}) ->
