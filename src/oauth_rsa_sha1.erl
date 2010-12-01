3f5b27276c569b176f439988f4478fbde3b0da68
Fernando Benavides <fernando.benavides@inakanetworks.com>
2010-12-01 11:14:10 -0300

    Use public_key-0.8 API (R14B). (by tim)

--- src/oauth_rsa_sha1.erl
+++ src/oauth_rsa_sha1.erl
@@ -6,28 +6,27 @@
 
 -spec signature(string(), string()) -> string().
 signature(BaseString, PrivateKeyPath) ->
-	{ok, Contents} = file:read_file(PrivateKeyPath),
+  {ok, Contents} = file:read_file(PrivateKeyPath),
   [Info] = public_key:pem_decode(Contents),
   PrivateKey = public_key:pem_entry_decode(Info),
-  base64:encode_to_string(public_key:sign(list_to_binary(BaseString), PrivateKey)).
+  base64:encode_to_string(public_key:sign(list_to_binary(BaseString), sha, PrivateKey)).
 
 -spec verify(string(), string(), term()) -> boolean().
-verify(Signature, BaseString, PublicKey) ->
-  public_key:verify_signature(to_binary(BaseString), sha, base64:decode(Signature), public_key(PublicKey)).
+verify(Signature, BaseString, Cert) ->
+  public_key:verify(to_binary(BaseString), sha, base64:decode(Signature), pkey(Cert)).
 
 to_binary(Term) when is_list(Term) ->
   list_to_binary(Term);
 to_binary(Term) when is_binary(Term) ->
   Term.
 
-public_key(Path) when is_list(Path) ->
-	{ok, Contents} = file:read_file(Path),
-  [{cert, DerCert, not_encrypted}] = public_key:pem_decode(Contents),
-  {ok, Cert} = public_key:pkix_decode_cert(DerCert, otp),
-  public_key(Cert);
-public_key(#'OTPCertificate'{tbsCertificate=Cert}) ->
-  public_key(Cert);
-public_key(#'OTPTBSCertificate'{subjectPublicKeyInfo=Info}) ->
-  public_key(Info);
-public_key(#'OTPSubjectPublicKeyInfo'{subjectPublicKey=Key}) ->
+pkey(Path) when is_list(Path) ->
+  {ok, Contents} = file:read_file(Path),
+  [{'Certificate', DerCert, not_encrypted}] = public_key:pem_decode(Contents),
+  pkey(public_key:pkix_decode_cert(DerCert, otp));
+pkey(#'OTPCertificate'{tbsCertificate=Cert}) ->
+  pkey(Cert);
+pkey(#'OTPTBSCertificate'{subjectPublicKeyInfo=Info}) ->
+  pkey(Info);
+pkey(#'OTPSubjectPublicKeyInfo'{subjectPublicKey=Key}) ->
   Key.
